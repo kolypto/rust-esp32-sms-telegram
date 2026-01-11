225d3e706f45482e5595ce68cbe2dbbb5ab1522e
Mark Vartanyan <kolypto@gmail.com>
2026-01-11 03:01:30 +0300

    esp-generate

---
+++ README.md
@@ -0,0 +1,14 @@
+Rust-ESP32 SMS-Telegram Bridge
+==============================
+
+Hardware:
+
+* ESP32-C3
+* SIM800L modem on UART
+
+Software:
+
+* WiFI
+* Telegram bot
+* Channel to forward SMS to
+
---
+++ sms-tg/src/bin/main.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![no_main]
+#![deny(clippy::mem_forget)]
+#![deny(clippy::large_stack_frames)]
+extern crate alloc;
+use {esp_backtrace as _, esp_println as _};
+esp_bootloader_esp_idf::esp_app_desc!();
+
+use defmt;
+use esp_hal::{
+    clock::CpuClock,
+    timer::timg::TimerGroup,
+    interrupt::software::SoftwareInterruptControl,
+};
+
+use embassy_executor::Spawner;
+use embassy_time::{
+    Timer,
+};
+
+#[allow(clippy::large_stack_frames)]
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // Init allocator: 64K in reclaimed memory + 66K in default RAM
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 64 * 1024);
+    esp_alloc::heap_allocator!(size: 72 * 1024);
+
+    // CPU Clock: WiFi in ESP32 requires a fast CPU
+    let peripherals = esp_hal::init(esp_hal::Config::default().with_cpu_clock(CpuClock::max()));
+
+    // Init Embassy the usual way
+    let sw_int = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0, sw_int.software_interrupt0);
+
+    // // Init WiFi & network stack
+    // let stack = defmt::expect!(
+    //     pokakus::wifi::start_wifi(&spawner, peripherals.WIFI).await,
+    //     "Init WiFi"
+    // );
+
+    // Spawn some tasks
+    // spawner.must_spawn(...);
+
+    loop {
+        Timer::after_secs(1).await;
+    }
+}
